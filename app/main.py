e99fed99fc416492f835efa9accba5cea9b00eb8
faridasLab <finance.labs1@gmail.com>
2022-04-11 18:04:38 +0400

    update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_utils.tasks import repeat_every
 from .routers import user, auth, available_data, watchlist
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -22,43 +23,36 @@ app.add_middleware(
 )
 
 class BackgroundRunner:
-    def __init__(self):
-        self.updated = False
-
+    
     async def update_signals(self):
-        while True:
-            await asyncio.sleep(0.1)
-            if self.updated is False:
-                if dt.now().hour == 22:
-                    session = Session(engine)
-                    signal_query = session.query(models.Crypto).all()
+        await asyncio.sleep(0.1)
+        if dt.now().hour == 22:
+            session = Session(engine)
+            signal_query = session.query(models.Crypto).all()
+        
+            for s in signal_query:
+                #  Check if the crypto does not appear in anyone's watchlist, remove it from the database
+                on_watchlist = session.query(models.Watchlist).filter(models.Watchlist.crypto_id == s.id).first
+                crypto = session.execute(select(models.Crypto).filter_by(id=s.id)).scalar_one()
+        
+                if on_watchlist:
+                    pair = s.name
+                    exchange = s.exchange
+                    signal_stage = calculate_signals(pair, exchange)
+                    crypto.signal_stage = signal_stage
+                    session.commit()
+                else:
+                    crypto.delete(synchronize_session=False)
+                    session.commit()
+        
+            session.close()
 
-                    for s in signal_query:
-                        #  Check if the crypto does not appear in anyone's watchlist, remove it from the database
-                        on_watchlist = session.query(models.Watchlist).filter(models.Watchlist.crypto_id == s.id).first
-                        crypto = session.execute(select(models.Crypto).filter_by(id=s.id)).scalar_one()
-
-                        if on_watchlist:
-                            pair = s.name
-                            exchange = s.exchange
-                            signal_stage = calculate_signals(pair, exchange)
-                            crypto.signal_stage = signal_stage
-                            session.commit()
-                        else:
-                            crypto.delete(synchronize_session=False)
-                            session.commit()
-
-                    session.close()
-                    self.updated = True
-
-            else:
-                if dt.now().hour == 10:
-                    self.updated = False
 
 
 runner = BackgroundRunner()
 
 @app.on_event('startup')
+@repeat_every(seconds=60*60*24) 
 async def app_startup():
     asyncio.create_task(runner.update_signals())
 
