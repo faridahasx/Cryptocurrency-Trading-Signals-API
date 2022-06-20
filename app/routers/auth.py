90703276a8a082218fbd32e6707278f7c65b7732
Farida Hasanova <99580486+faridasLab@users.noreply.github.com>
2022-06-20 09:14:39 +0400

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,4 @@
 import os
-
 from fastapi import APIRouter, Depends, status, HTTPException,Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -10,7 +9,6 @@ router = APIRouter(tags=['Authentication'])
 
 @router.post('/login', response_model=schemas.Token)
 def user_login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
-
     user = db.query(models.User).filter(
         models.User.email == user_credentials.username).first()
 
